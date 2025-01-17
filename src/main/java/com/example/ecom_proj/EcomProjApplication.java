30916664da67af0166c02302b78c86ad7b821af4
Duleepa Wickramasinghe <duleepa@asiapacific.tech>
2025-01-17 10:38:44 +0530

    Added logs and unit tests.

--- src/main/java/com/example/ecom_proj/EcomProjApplication.java
+++ src/main/java/com/example/ecom_proj/EcomProjApplication.java
@@ -1,12 +1,25 @@
 package com.example.ecom_proj;
 
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class EcomProjApplication {
+	
+	public static Logger logger = LoggerFactory.getLogger(SpringApplication.class);
 
+	@PostConstruct
+	public void intt() {
+		logger.info("Application Started..");
+	}
+	
 	public static void main(String[] args) {
+		logger.info("Application executed...");
 		SpringApplication.run(EcomProjApplication.class, args);
 	}
 
--- src/test/java/com/example/ecom_proj/EcomProjApplicationTests.java
+++ src/test/java/com/example/ecom_proj/EcomProjApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.ecom_proj;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EcomProjApplicationTests {
+	
+	public static Logger logger = LoggerFactory.getLogger(EcomProjApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing.");
+		assertEquals(true, true);
 	}
 
 }
